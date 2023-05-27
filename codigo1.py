5084853c3417576467d86db839a8c1c3f37b8695
Akaaniss <annaismonserrat.skz@gmail.com>
2023-05-27 16:42:30 -0400

    Update codigo1.py

--- codigo1.py
+++ codigo1.py
@@ -1,36 +1,48 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QTableWidget, QTableWidgetItem
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QPushButton
+from PyQt6 import QtCore, QtGui
+
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def _init_(self):
+        super()._init_()
         self.setWindowTitle("Sistema de almacenamiento")
-        self.setGeometry(200,200,800,600)
+        self.setGeometry(200, 200, 800, 600)
 
-        self.inventory_button = QPushButton("Inventario")
-        self.inventory_button.clicked.connect(self.open_inventory)
+        self.label = QLabel("¿Qué desea hacer?")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.visualize_button = QPushButton("Visualizar productos")
+        self.visualize_button.clicked.connect(self.open_inventory)
+
+        self.modify_button = QPushButton("Añadir o eliminar productos")
+        self.modify_button.clicked.connect(self.open_modify_window)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.inventory_button)
+        layout.addWidget(self.label)
+        layout.addWidget(self.visualize_button)
+        layout.addWidget(self.modify_button)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
-
-        self.show()
     def open_inventory(self):
         inventory_window = InventoryWindow()
         inventory_window.show()
         self.hide()
 
+    def open_modify_window(self):
+        modify_window = ModifyWindow()
+        modify_window.show()
+        self.hide()
+
 
 class InventoryWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+    def _init_(self):
+        super()._init_()
         self.setWindowTitle("Inventario")
-        self.setGeometry(200,200,600,400)
+        self.setGeometry(200, 200, 600, 400)
 
         self.back_button = QPushButton("Volver")
         self.back_button.clicked.connect(self.go_back)
@@ -40,8 +52,7 @@ class InventoryWindow(QWidget):
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-        self.show()
+        self.setLayout(layout)
 
     def go_back(self):
         main_window = MainWindow()
@@ -49,7 +60,30 @@ class InventoryWindow(QWidget):
         self.hide()
 
 
-if __name__ == '__main__':
-        app = QApplication(sys.argv)
-        window = MainWindow()
-        sys.exit(app.exec())
+class ModifyWindow(QWidget):
+    def _init_(self):
+        super()._init_()
+        self.setWindowTitle("Añadir o eliminar productos")
+        self.setGeometry(200, 200, 600, 400)
+
+        self.back_button = QPushButton("Volver")
+        self.back_button.clicked.connect(self.go_back)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.back_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setLayout(layout)
+
+    def go_back(self):
+        main_window = MainWindow()
+        main_window.show()
+        self.hide()
+
+
+if __name__ == '_main_':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
