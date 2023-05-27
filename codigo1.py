e5b8e5301ba1388c2a8a0e11a08615e90a82a60d
Akaaniss <annaismonserrat.skz@gmail.com>
2023-05-27 17:42:52 -0400

    jijijij

--- codigo1.py
+++ codigo1.py
@@ -32,22 +32,28 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+        self.inventory_window = None
+        self.modify_window = None
+
     def open_inventory(self):
-        inventory_window = InventoryWindow()
-        inventory_window.set_data("Información de visualización de productos")  
-        inventory_window.show()
+        if self.inventory_window is None:
+            self.inventory_window = InventoryWindow(self)
+            self.inventory_window.set_data("Información de visualización de productos")
+        self.inventory_window.show()
         self.hide()
 
     def open_modify_window(self):
-        modify_window = ModifyWindow()
-        modify_window.set_data("Información para añadir o eliminar productos")  
-        modify_window.show()
+        if self.modify_window is None:
+            self.modify_window = ModifyWindow(self)
+            self.modify_window.set_data("Información para añadir o eliminar productos")
+        self.modify_window.show()
         self.hide()
 
 
 class InventoryWindow(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
+        self.main_window = main_window
         self.setWindowTitle("Inventario")
         self.setGeometry(200, 200, 600, 400)
 
@@ -57,6 +63,7 @@ class InventoryWindow(QWidget):
         self.data_label = QLabel()
 
         layout = QVBoxLayout()
+        layout.addWidget(self.data_label)
         layout.addWidget(self.back_button)
 
         central_widget = QWidget()
@@ -67,14 +74,14 @@ class InventoryWindow(QWidget):
         self.data_label.setText(data) 
 
     def go_back(self):
-        main_window = MainWindow()
-        main_window.show()
+        self.main_window.show()
         self.hide()
 
 
 class ModifyWindow(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
+        self.main_window = main_window
         self.setWindowTitle("Añadir o eliminar productos")
         self.setGeometry(200, 200, 600, 400)
 
@@ -84,6 +91,7 @@ class ModifyWindow(QWidget):
         self.data_label = QLabel()
 
         layout = QVBoxLayout()
+        layout.addWidget(self.data_label)
         layout.addWidget(self.back_button)
 
         central_widget = QWidget()
@@ -94,8 +102,7 @@ class ModifyWindow(QWidget):
         self.data_label.setText(data)
 
     def go_back(self):
-        main_window = MainWindow()
-        main_window.show()
+        self.main_window.show()
         self.hide()
 
 
