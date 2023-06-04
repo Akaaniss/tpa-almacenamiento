9bea8221b825011216e7f8e12e52585c667487eb
Akaaniss <annaismonserrat.skz@gmail.com>
2023-06-03 20:23:18 -0400

    Update mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,54 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow, QWidget, QLabel, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QWidget, QLabel, QVBoxLayout, QPushButton,QLineEdit,QMessageBox
 from PyQt6 import QtCore
 from modifyWindow import ModifyWindow
 from inventoryWindow import InventoryWindow
+
+class loginWindow(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setWindowTitle("Inicio de sesión")
+        self.setGeometry(200, 200, 400, 300)
+
+        self.login_label = QLabel("Inicio de sesión")
+        self.login_label.setStyleSheet("font-size: 24px; margin-bottom: 20px;")
+
+        self.username_label = QLabel("Usuario:")
+        self.password_label = QLabel("Contraseña:")
+
+        self.username_input = QLineEdit()
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+
+        self.login_button = QPushButton("Iniciar sesión")
+        self.login_button.clicked.connect(self.login)
+        self.login_button.setStyleSheet("font-size: 18px; padding: 10px 20px;")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.login_label)
+        layout.addWidget(self.username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.login_button)
+        layout.addStretch()
+
+        self.setLayout(layout)
+
+    def login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+
+        if username == "admin" and password == "admin":
+            print("Inicio de sesión exitoso")
+            self.parent().login_successful()
+        else:
+            error_dialog = QMessageBox()
+            error_dialog.setIcon(QMessageBox.Icon.Critical)
+            error_dialog.setWindowTitle("Error de inicio de sesión")
+            error_dialog.setText("Usuario o contraseña incorrectos.")
+            print("Inicio de sesión fallido")
+            error_dialog.exec()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -37,6 +83,11 @@ class MainWindow(QMainWindow):
         self.inventory_window = None
         self.modify_window = None
 
+    def login_successful(self):
+        self.label.setText("¿Qué desea hacer?")
+        self.visualize_button.setEnabled(True)
+        self.modify_button.setEnabled(True)
+
     def open_inventory(self):
         if self.inventory_window is None:
             self.inventory_window = InventoryWindow(self)
@@ -50,3 +101,4 @@ class MainWindow(QMainWindow):
             self.modify_window.set_data("Información para añadir o eliminar productos")
         self.modify_window.show()
         self.hide()
+    
